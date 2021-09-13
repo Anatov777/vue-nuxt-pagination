ba4b135c69505e43265c08a01fb6201d18df02f5
Anatov777 <anatovtimur@gmail.com>
2021-09-13 07:45:54 +0300

    Updates

---
+++ _nuxt/static/1631508287/catalog/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fvue-nuxt-pagination\u002F_nuxt\u002Fstatic\u002F1631508287",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcatalog",config:{_app:{basePath:"\u002Fvue-nuxt-pagination\u002F",assetsPath:"\u002Fvue-nuxt-pagination\u002F_nuxt\u002F",cdnURL:a}}}}(null));
